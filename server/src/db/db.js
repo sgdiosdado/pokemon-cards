a97312c1f45e9af1aeb9f725fa15386ccb6b4c11
Sergio Diosdado <sgdc_98@hotmail.com>
2020-10-03 10:05:44 -0500

    Database connection options

--- server/src/db/db.js
+++ server/src/db/db.js
@@ -2,9 +2,17 @@ const mongoose = require("mongoose");
 const dbUser = process.env.DB_USERNAME;
 const dbPassword = process.env.DB_PASSWORD;
 const connectionString = `mongodb://${dbUser}:${dbPassword}@mongo:27017/`;
+
 // Databse connection
+const options = {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  autoIndex: true,
+  useUnifiedTopology: true,
+}
+
 mongoose
-  .connect(connectionString, { useNewUrlParser: true, autoIndex: true })
+  .connect(connectionString, options)
   .then((_) => console.log("Connection successful!"))
   .catch((err) => console.log(`Connection error: ${err.message}`));
 const db = mongoose.connection;
